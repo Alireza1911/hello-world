8a2e5cc27e2028231859d8355c246305e658483f
alireza <39245095+Alireza1911@users.noreply.github.com>
2018-05-28 01:15:35 +0430

    Add files via upload

---
+++ StackGen/StackGen/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//معرفی ثوابت
+            int andis_ofint = 0, andis_ofstring = 0, andis_ofclass = 0;
+            //معرفی ارایه جهت استک اینت
+            int[] arrayint = null;
+            //معرفی ارایه جهت استک استرینگ
+            string[] arraystring = null;
+            //ساخت کلاس و ارایه جهت استک کلاس
+            StackClass c = new StackClass();
+            StackClass d = new StackClass();
+            StackClass[] arrayclass = null;
+            //ساخت کلاس جهت رفتارهای مرتبط با استک 
+            StackGenericeClass a = new StackGenericeClass();
+            //int
+            arrayint = a.Push(4, ref andis_ofint, arrayint);
+            arrayint = a.Push(7, ref andis_ofint, arrayint);
+            arrayint = a.Pop(ref andis_ofint, arrayint);
+            int peckint = a.Peck(arrayint, andis_ofint);
+            //string
+            arraystring = a.Push("ali", ref andis_ofstring, arraystring);
+            arraystring = a.Push("reza", ref andis_ofstring, arraystring);
+            arraystring = a.Pop(ref andis_ofstring, arraystring);
+            string peckstring = a.Peck(arraystring, andis_ofint);
+            //class
+            arrayclass = a.Push(c, ref andis_ofclass, arrayclass);
+            arrayclass = a.Push(d, ref andis_ofclass, arrayclass);
+            arrayclass = a.Pop(ref andis_ofclass,arrayclass);
+            StackClass peckclass = a.Peck(arrayclass, andis_ofclass);
+
+        }
+    }
+}
---
+++ StackGen/StackGen/StackGenericeClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackGen
+{
+   public class StackGenericeClass
+    {
+        //متد پوش
+        public T[] Push<T>(T input, ref int m, T[] a)
+        {
+            m++;
+            T[] b = new T[m];
+            if (m == 1)
+            {
+                b[0] = input;
+            }
+            else
+            {
+                for (int i = 0; i < (b.Length) - 1; i++)
+                {
+                    b[i] = a[i];
+                }
+                b[m - 1] = input;
+            }
+            return b;
+        }
+        //متد پاپ
+        public T[] Pop<T>(ref int m, T[] a)
+        {
+            T[] b;
+            if (m < 1)
+            {
+                Console.WriteLine("stack is empty");
+            }
+            else
+            {
+                m--;
+                b = new T[m];
+                for (int i = 0; i < b.Length; i++)
+                {
+                    b[i] = a[i];
+                }
+                a = b;
+            }
+            return a;
+        }
+        public T Peck<T>(T[] a, int m)
+        {
+            return a[m - 1];
+        }
+    }
+}
